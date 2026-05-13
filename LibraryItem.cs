7f5215d5e878a62377f0cbf0baf22302f805305d
khedr-prog <khedralkhateeb@gmail.com>
2026-05-14 02:42:20 +0300

    Add abstract class LibraryItem

---
+++ LibraryItem.cs
@@ -0,0 +1,26 @@
+﻿namespace LibraryApp
+{
+    public abstract class LibraryItem : IItem
+    {
+        public int Id { get; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        private static int nextId = 1;
+
+        public LibraryItem(string title, int year)
+        {
+            Id = nextId++;
+            Title = title;
+            Year = year;
+        }
+
+        public abstract void DisplayInfo();
+
+        public virtual bool Validate()
+        {
+            return !string.IsNullOrWhiteSpace(Title) && Year > 0;
+        }
+
+    }
+}
