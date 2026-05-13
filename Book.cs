7a0a9c3a0c59f4e7b2e40289acd4c158133b58ee
khedr-prog <khedralkhateeb@gmail.com>
2026-05-14 02:43:20 +0300

    Add book class

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LibraryApp
+{
+    public class Book : LibraryItem
+    {
+        public string Author { get; set; }
+
+        public Book(string title, int year, string author)
+            : base(title, year)
+        {
+            Author = author;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Book: {Title} - {Author} ({Year})");
+        }
+        public override bool Validate()
+        {
+            return ValidationHelper.IsValidTitle(Title)
+                && ValidationHelper.IsValidYear(Year)
+                && ValidationHelper.IsValidAuthor(Author);
+        }
+    }
+}
