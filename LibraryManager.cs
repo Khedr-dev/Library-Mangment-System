79f79043cfb14e3afe4c416526c3c73973dddc25
Haidara Alzeer <62031430+HaidarA999@users.noreply.github.com>
2026-05-14 08:40:32 -0700

    add LibraryManager.cs

---
+++ LibraryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryApp
+{
+    public class LibraryManager
+    {
+        private List<LibraryItem> items = new List<LibraryItem>();
+
+        public static int TotalItems { get; private set; } = 0;
+
+        public delegate void ItemAddedHandler(LibraryItem item);
+        public event ItemAddedHandler ItemAdded;
+
+        public void AddItem(LibraryItem item)
+        {
+            if (item.Validate())
+            {
+                items.Add(item);
+                TotalItems++;
+                ItemAdded?.Invoke(item);
+            }
+        }
+
+        public void ShowAll()
+        {
+            foreach (var item in items)
+            {
+                item.DisplayInfo();
+            }
+        }
+    }
+}
