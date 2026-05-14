06d023b4770178b2eee81b03d603a0a4c63c26cd
Haidara Alzeer <62031430+HaidarA999@users.noreply.github.com>
2026-05-14 08:39:16 -0700

    add Magazine.cs

---
+++ Magazine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LibraryApp
+{
+    public class Magazine : LibraryItem
+    {
+        public int IssueNumber { get; set; }
+
+        public Magazine(string title, int year, int issueNumber)
+            : base(title, year)
+        {
+            IssueNumber = issueNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Magazine: {Title} - Issue {IssueNumber} ({Year})");
+        }
+        public override bool Validate()
+        {
+            return ValidationHelper.IsValidTitle(Title)
+                && ValidationHelper.IsValidYear(Year)
+                && ValidationHelper.IsValidIssue(IssueNumber);
+        }
+    }
+}
